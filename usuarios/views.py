f38f0e05e361ea406ebd977c8028f67b96af9585
Luis Fernando Brasil <luis4756@live.com>
2022-08-31 10:22:14 -0300

    Adcionado registro de usuários

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
-
+from django.urls import reverse_lazy
+from django.views.generic.edit import CreateView
+from django.contrib.auth.models import User
+from .forms import UsuarioForm
 # Create your views here.
+
+class UsuarioCreate(CreateView):
+    template_name = "usuarios/form_registrar.html"
+    form_class = UsuarioForm
+    sucess_url = reverse_lazy('login')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['titulo'] = 'Cadastro de Usuário'
+        context['botao'] = 'Cadastrar'
+        context['cor'] = 'success'
+        return context 
